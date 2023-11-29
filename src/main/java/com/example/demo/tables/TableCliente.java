efce6bbd89bdb5dce8de0e42c57b357fa39bc180
Vitor <vcamposprofissional@gmail.com>
2023-11-29 17:49:14 -0300

    Inicio Table Cliente

--- src/main/java/com/example/demo/tables/TableCliente.java
+++ src/main/java/com/example/demo/tables/TableCliente.java
@@ -1,4 +1,26 @@
 package com.example.demo.tables;
 
+
+
 public class TableCliente {
+
+
+    private int Id;
+
+    private String criado_em;
+
+    private String atualizado_em;
+
+    private String razao_social;
+
+    private String nome_fantasia;
+
+    private String data_nascimento;
+
+    private String type;
+
+    private String document;
+
+    private int
+
 }
