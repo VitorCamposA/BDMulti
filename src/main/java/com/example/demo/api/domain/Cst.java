63184c9b22d6416400b9d6fa03a46390a8560a79
Vitor <vcamposprofissional@gmail.com>
2023-12-01 16:24:35 -0300

    Criacao table Cst

---
+++ src/main/java/com/example/demo/api/domain/Cst.java
@@ -0,0 +1,42 @@
+package com.example.demo.api.domain;
+
+import com.example.demo.api.domain.base.AbstractBaseId;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+
+@Table(name = "cst")
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Cst extends AbstractBaseId {
+
+    @Column
+    private String descricao;
+
+    @Column(name = "cst_ism")
+    private Long cstIsm;
+
+    @Column(name = "perc_ism")
+    private BigDecimal percIsm;
+
+    @Column(name = "cst_pis")
+    private Long cstPis;
+
+    @Column(name = "perc_pis")
+    private BigDecimal percPis;
+
+    @Column(name = "cst_cofins")
+    private Long cstCofins;
+
+    @Column(name = "perc_cofins")
+    private BigDecimal percCofins;
+
+    //FK VVV
+
+    @Column
+    private Empresa empresa;
+
+}
